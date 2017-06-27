934215b0ac9e2104906355778e5ad1d39bed6261
KYUNG MO KWEON <kkweon@gmail.com>
2017-06-26 21:31:44 -0700

    hotfix: gitconfig

--- git_workspace_install.sh
+++ git_workspace_install.sh
@@ -3,4 +3,5 @@ cp .bashrc .bashrc.backup
 git clone https://github.com/kkweon/git-workspace-setup.git .git
 cat .git/add_this_to_bash_profile >> .bashrc
 source .bashrc
+cp .git/gitconfig .gitconfig
 echo "Open .gitconfig change name and email"
